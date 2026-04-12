fc6edb68196a3657e2b70f82c57a4170babfc8ce
Samuel Gomes <samuel.gomes.k276@gmail.com>
2026-04-12 02:55:03 +0100

    refactor: merge App.tsx into main.tsx, remove app/ directory
    Inline the App component into main.tsx, move global.scss
    to shared/styles/. One less directory and file.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from '@/app/App';
-import '@/app/global.scss';
+import { BrowserRouter as Router, Routes as RouterRoutes, Route } from 'react-router-dom';
+import { Home } from '@/features/map/Home';
+import { Header } from '@/layout/Header';
+import { Footer } from '@/layout/Footer';
+import { StationDetail } from '@/features/stations/StationDetail';
+import { TrainDetail } from '@/features/trains/TrainDetail';
+import { Alerts } from '@/features/alerts/Alerts';
+import { PlanearViagem } from '@/features/planner/PlanearViagem';
+import { TrainProvider } from '@/shared/contexts/TrainContext';
+import { Routes } from '@/shared/routes';
+import '@/shared/styles/global.scss';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <TrainProvider>
+        <Header />
+        <main className='main-content'>
+          <RouterRoutes>
+            <Route path={Routes.HOME} element={<Home />} />
+            <Route path={Routes.ALERTS} element={<Alerts />} />
+            <Route path={Routes.PLANNER} element={<PlanearViagem />} />
+            <Route path={Routes.STATION} element={<StationDetail />} />
+            <Route path={Routes.TRAIN} element={<TrainDetail />} />
+            <Route path='*' element={<div>404 Not Found</div>} />
+          </RouterRoutes>
+        </main>
+        <Footer />
+      </TrainProvider>
+    </Router>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
